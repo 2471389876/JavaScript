ce273e729d0b1124892ba5a8904ada4d22227293
2471389876 <2471389876@qq.com>
2021-04-09 09:31:43 +0800

    6end

--- DOM/scripts/showPic.js
+++ DOM/scripts/showPic.js
@@ -1,13 +1,59 @@
 function showPic(whichpic) {
-    var placeholder = document.getElementById("placeholder");
+    if (!document.getElementById("placeholder")) {
+        return false;
+    }
     var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    if (placeholder.nodeName != "IMG") {
+        return false;
+    }
+    console.log(source);
     placeholder.setAttribute("src", source);
-    var text = whichpic.getAttribute("title");
-    var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+    if (document.getElementById("description")) {
+        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+        var description = document.getElementById("description");
+        if (description.firstChild.nodeValue == 3) {
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
 }
 function countBodyChildren() {
     var body_element = document.getElementsByTagName("body")[0];
     alert(body_element.nodeType);
 }
 //window.onload = countBodyChildren;
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) {
+        return false;
+    }
+    if (!document.getElementById) {
+        return false;
+    }
+    if (!document.getElementById("imagegallery")) {
+        return false;
+    }
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            // return showPic(this) ? true : false;
+            return showPic(this) ? false : true;
+        }
+    }
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
