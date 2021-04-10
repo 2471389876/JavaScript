90a8823304e00cdf271c0d438bb237b48de0fcee
2471389876 <2471389876@qq.com>
2021-04-10 21:39:23 +0800

    初始vue

---
+++ VUE/demo1.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: '黑马程序员',
+        school: {
+            name: "学校",
+            mobile: 100
+        },
+        arr: ['北京', '南京', '上海']
+
+    }
+})
